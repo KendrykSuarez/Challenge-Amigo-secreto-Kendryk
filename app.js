396a0f7cf9e1d881ec8a0a2d4a46acc9bc3607c7
KendrykSuarez <kendrykalejandrosuarezcuello@gmail.com>
2025-03-07 17:44:46 +0000

    amigo secreto terminado

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function amigosIngresados() {
+    let nombre = document.getElementById('amigo');
+    let nombreLimpio = nombre.value.trim(); 
+
+ 
+    if (nombreLimpio === "") {
+        alert("El Nombre no debe estar vacío");
+        return; 
+    }
+
+    listaDeAmigos.push(nombreLimpio);
+
+    let li = document.createElement('li');
+    li.textContent = nombreLimpio;
+    document.getElementById('listaAmigos').appendChild(li);
+
+  
+    nombre.value = '';
+}
+
+function sortearAmigos() {
+ 
+    if (listaDeAmigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let amigoSorteado = listaDeAmigos[indiceAleatorio];
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = ''; // Limpiar resultado anterior
+    let li = document.createElement('li');
+    li.textContent = `El amigo sorteado es: ${amigoSorteado}`;
+    resultado.appendChild(li);
+}
+
+
+document.querySelector(".button-add").addEventListener("click", amigosIngresados);
+document.querySelector(".button-draw").addEventListener("click", sortearAmigos);
